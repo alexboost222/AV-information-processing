7ed647a25cb2ebd5d55bd0ead504c50e5a80580e
alexboost222 <alexboost222@gmail.com>
2021-02-10 23:44:48 +0300

    add: some additional logic for first lab task

--- first_lab/first_lab_logic.py
+++ first_lab/first_lab_logic.py
@@ -7,13 +7,13 @@ def closest_neighbor_upsampling(image, to_width, to_height):
 
     if not is_image:
         raise TypeError(f"Object passed to function has type {image.__class__}."
-                        "Expected that it will be subclass of Image")
+                        "Expected that it will be subclass of Image.")
 
     if image.width > to_width:
-        raise ValueError(f"Passed image width {image.width} > to_width {to_width}, but <= expected")
+        raise ValueError(f"Passed image width {image.width} > to_width {to_width}, but <= expected.")
 
     if image.height > to_height:
-        raise ValueError(f"Passed image height {image.height} > to_height {to_height}, but <= expected")
+        raise ValueError(f"Passed image height {image.height} > to_height {to_height}, but <= expected.")
 
     new_pixels = [None for _ in range(to_width * to_height)]
 
@@ -34,13 +34,13 @@ def decimation_downsampling(image, to_width, to_height):
 
     if not is_image:
         raise TypeError(f"Object passed to function has type {image.__class__}."
-                        "Expected that it will be subclass of Image")
+                        "Expected that it will be subclass of Image.")
 
     if image.width < to_width:
-        raise ValueError(f"Passed image width {image.width} < to_width {to_width}, but >= expected")
+        raise ValueError(f"Passed image width {image.width} < to_width {to_width}, but >= expected.")
 
     if image.height < to_height:
-        raise ValueError(f"Passed image height {image.height} < to_height {to_height}, but >= expected")
+        raise ValueError(f"Passed image height {image.height} < to_height {to_height}, but >= expected.")
 
     new_pixels = [None for _ in range(to_width * to_height)]
     decimated_lines = random_sample(range(image.height), image.height - to_height)
@@ -64,12 +64,49 @@ def decimation_downsampling(image, to_width, to_height):
     return result
 
 
+def upsample_integer_number_of_times(image, k):
+    is_image = isinstance(image, Image.Image)
+
+    if not is_image:
+        raise TypeError(f"Object passed to function has type {image.__class__}."
+                        "Expected that it will be subclass of Image.")
+
+    if not isinstance(k, int):
+        raise TypeError(f"Object passed to function has type {k.__class__}."
+                        "Expected that it will be int.")
+
+    if k < 1:
+        raise TypeError(f"Param k passed to method has value {k} < 1. Expected >= 1.")
+
+    return closest_neighbor_upsampling(image, image.width * k, image.height * k)
+
+
+def downsample_integer_number_of_times(image, n):
+    is_image = isinstance(image, Image.Image)
+
+    if not is_image:
+        raise TypeError(f"Object passed to function has type {image.__class__}."
+                        "Expected that it will be subclass of Image.")
+
+    if not isinstance(n, int):
+        raise TypeError(f"Object passed to function has type {n.__class__}."
+                        "Expected that it will be int.")
+
+    if n < 1:
+        raise TypeError(f"Param n passed to method has value {n} < 1. Expected >= 1.")
+
+    return decimation_downsampling(image, image.width // n, image.height // n)
+
+
 image_path = 'images/eye.png'
 resized_image_path = 'images/eye_resized.png'
 
 new_width = 100
 new_height = 100
 
+up_coef = 3
+down_coef = 2
+
 im = Image.open(image_path)
-resized_im = decimation_downsampling(im, new_width, new_height)
+resized_im = downsample_integer_number_of_times(upsample_integer_number_of_times(im, up_coef), down_coef)
 resized_im.save(resized_image_path)
