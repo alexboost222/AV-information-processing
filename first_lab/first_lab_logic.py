e165f16457107db6936c3001f61c510e732476ba
alexboost222 <alexboost222@gmail.com>
2021-02-10 22:56:59 +0300

    add: decimation and correct image param type check

--- first_lab/first_lab_logic.py
+++ first_lab/first_lab_logic.py
@@ -1,12 +1,12 @@
 from PIL import Image
+from random import sample as random_sample
 
 
-def closest_neighbor_interpolation(image, to_width, to_height):
-    image_type = type(image)
-    is_image = issubclass(image_type, type(Image))
+def closest_neighbor_upsampling(image, to_width, to_height):
+    is_image = isinstance(image, Image.Image)
 
-    if is_image:
-        raise TypeError(f"Object passed to function has type {image_type}. Expected that it will be subclass of Image")
+    if not is_image:
+        raise TypeError(f"Object passed to function has type {image.__class__}. Expected that it will be subclass of Image")
 
     if image.width > to_width:
         raise ValueError(f"Passed image width {image.width} > to_width {to_width}, but <= expected")
@@ -20,7 +20,7 @@ def closest_neighbor_interpolation(image, to_width, to_height):
         y_nearest = int(i * image.height / to_height)
         for j in range(to_width):
             x_nearest = int(j * image.width / to_width)
-            new_pixels[i * to_width + j] = (image.getpixel((x_nearest, y_nearest)))
+            new_pixels[i * to_width + j] = image.getpixel((x_nearest, y_nearest))
 
     result = Image.new(image.mode, (to_width, to_height))
     result.putdata(new_pixels)
@@ -28,12 +28,46 @@ def closest_neighbor_interpolation(image, to_width, to_height):
     return result
 
 
-image_path = 'images/5x5.png'
-resized_image_path = 'images/5x5_resized.png'
+def decimation_downsampling(image, to_width, to_height):
+    is_image = isinstance(image, Image.Image)
 
-new_width = 735
-new_height = 61
+    if not is_image:
+        raise TypeError(f"Object passed to function has type {image.__class__}. Expected that it will be subclass of Image")
+
+    if image.width < to_width:
+        raise ValueError(f"Passed image width {image.width} < to_width {to_width}, but >= expected")
+
+    if image.height < to_height:
+        raise ValueError(f"Passed image height {image.height} < to_height {to_height}, but >= expected")
+
+    new_pixels = [None for _ in range(to_width * to_height)]
+    decimated_lines = random_sample(range(image.height), image.height - to_height)
+    decimated_columns = random_sample(range(image.width), image.width - to_width)
+
+    iterator = 0
+    for i in range(image.height):
+        if i in decimated_lines:
+            continue
+
+        for j in range(image.width):
+            if j in decimated_columns:
+                continue
+
+            new_pixels[iterator] = image.getpixel((i, j))
+            iterator += 1
+
+    result = Image.new(image.mode, (to_width, to_height))
+    result.putdata(new_pixels)
+
+    return result
+
+
+image_path = 'images/eye.png'
+resized_image_path = 'images/eye_resized.png'
+
+new_width = 100
+new_height = 100
 
 im = Image.open(image_path)
-resized_im = closest_neighbor_interpolation(im, new_width, new_height)
+resized_im = decimation_downsampling(im, new_width, new_height)
 resized_im.save(resized_image_path)
