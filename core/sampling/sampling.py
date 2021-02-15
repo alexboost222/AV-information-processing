4239c0efdea5b1a36106088c88ab38d6ab105a91
alexboost222 <alexboost222@gmail.com>
2021-02-15 18:30:10 +0300

    upd: changed sampling cycles order to width then height

--- core/sampling/sampling.py
+++ core/sampling/sampling.py
@@ -2,8 +2,6 @@ from PIL import Image
 from core.math import math
 from core.verification import verification
 
-# TODO change order of for cycles from height; width to width; height
-
 
 # TODO extract different scripts for different modes like RGB etc
 def closest_neighbor_upsampling(image, factor):
@@ -18,11 +16,11 @@ def closest_neighbor_upsampling(image, factor):
     result_height = image.height * factor
     result = Image.new(image.mode, (result_width, result_height))
 
-    for i in range(result_height):
-        y_nearest = int(i * image.height / result_height)
-        for j in range(result_width):
-            x_nearest = int(j * image.width / result_width)
-            result.putpixel((j, i), image.getpixel((x_nearest, y_nearest)))
+    for x in range(result_width):
+        x_nearest = int(x * image.width / result_width)
+        for y in range(result_height):
+            y_nearest = int(y * image.height / result_height)
+            result.putpixel((x, y), image.getpixel((x_nearest, y_nearest)))
 
     return result
 
@@ -40,19 +38,19 @@ def bilinear_interpolation_upsampling(image, factor):
     result_height = image.height * factor
     result = Image.new(image.mode, (result_width, result_height))
 
-    for i in range(result_height):
-        for j in range(result_width):
-            if (i // factor == 0) and (j // factor == 0):
-                result.putpixel((j, i), image.getpixel((i // factor, j // factor)))
+    for x in range(result_width):
+        for y in range(result_height):
+            if (x % factor == 0) and (y % factor == 0):
+                result.putpixel((x, y), image.getpixel((x // factor, y // factor)))
                 continue
 
-            prev_row_number = i // factor
+            prev_row_number = y // factor
             next_row_number = prev_row_number + 1
-            prev_col_number = j // factor
+            prev_col_number = x // factor
             next_col_number = prev_col_number + 1
 
             if (next_row_number >= image.height) or (next_col_number >= image.width):
-                result.putpixel((j, i), (0, 0, 0))
+                result.putpixel((x, y), (0, 0, 0))
                 continue
 
             prev_row_prev_col_pixel = image.getpixel((prev_col_number, prev_row_number))
@@ -60,10 +58,10 @@ def bilinear_interpolation_upsampling(image, factor):
             next_row_prev_col_pixel = image.getpixel((prev_col_number, next_row_number))
             next_row_next_col_pixel = image.getpixel((next_col_number, next_row_number))
 
-            upper_pseudo_pixel = math.rgb_color_lerp(prev_col_number, prev_row_prev_col_pixel, next_col_number, prev_row_next_col_pixel, j / factor)
-            lower_pseudo_pixel = math.rgb_color_lerp(prev_col_number, next_row_prev_col_pixel, next_col_number, next_row_next_col_pixel, j / factor)
+            upper_pseudo_pixel = math.rgb_color_lerp(prev_col_number, prev_row_prev_col_pixel, next_col_number, prev_row_next_col_pixel, x / factor)
+            lower_pseudo_pixel = math.rgb_color_lerp(prev_col_number, next_row_prev_col_pixel, next_col_number, next_row_next_col_pixel, x / factor)
 
-            result.putpixel((j, i), math.rgb_color_lerp(prev_row_number, upper_pseudo_pixel, next_row_number, lower_pseudo_pixel, i / factor))
+            result.putpixel((x, y), math.rgb_color_lerp(prev_row_number, upper_pseudo_pixel, next_row_number, lower_pseudo_pixel, y / factor))
 
     return result
 
@@ -81,19 +79,15 @@ def decimation_downsampling(image, factor):
     result_height = image.height // factor
     result = Image.new(image.mode, (result_width, result_height))
 
-    iterator = 0
-    for i in range(image.height - factor + 1):
-        if i % factor != 0:
+    for x in range(image.width - factor + 1):
+        if x % factor != 0:
             continue
 
-        for j in range(image.width - factor + 1):
-            if j % factor != 0:
+        for y in range(image.height - factor + 1):
+            if y % factor != 0:
                 continue
 
-            col = iterator % result_width
-            row = iterator // result_width
-            result.putpixel((col, row), image.getpixel((j, i)))
-            iterator += 1
+            result.putpixel((x // factor, y // factor), image.getpixel((x, y)))
 
     return result
 
@@ -111,8 +105,8 @@ def one_pass_resampling(image, upsample_factor, downsample_factor):
     result_height = image.height * upsample_factor // downsample_factor
     result = Image.new(image.mode, (result_width, result_height))
 
-    for i in range(result_width):
-        for j in range(result_height):
-            result.putpixel((i, j), image.getpixel((i * downsample_factor // upsample_factor, j * downsample_factor / upsample_factor)))
+    for x in range(result_width):
+        for y in range(result_height):
+            result.putpixel((x, y), image.getpixel((x * downsample_factor // upsample_factor, y * downsample_factor / upsample_factor)))
 
     return result
