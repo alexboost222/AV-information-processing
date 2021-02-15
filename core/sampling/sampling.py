2bef8f6ba5cfd765c42b16e7bc79080cb2cceb9d
alexboost222 <alexboost222@gmail.com>
2021-02-15 17:12:55 +0300

    upd: decimation logic

--- core/sampling/sampling.py
+++ core/sampling/sampling.py
@@ -20,16 +20,13 @@ def closest_neighbor_upsampling(image, factor):
 
     result_width = image.width * factor
     result_height = image.height * factor
-    result_pixels = [None for _ in range(result_width * result_height)]
+    result = Image.new(image.mode, (result_width, result_height))
 
     for i in range(result_height):
         y_nearest = int(i * image.height / result_height)
         for j in range(result_width):
             x_nearest = int(j * image.width / result_width)
-            result_pixels[i * result_width + j] = image.getpixel((x_nearest, y_nearest))
-
-    result = Image.new(image.mode, (result_width, result_height))
-    result.putdata(result_pixels)
+            result.putpixel((j, i), image.getpixel((x_nearest, y_nearest)))
 
     return result
 
@@ -51,12 +48,12 @@ def bilinear_interpolation_upsampling(image, factor):
 
     result_width = image.width * factor
     result_height = image.height * factor
-    result_pixels = [None for _ in range(result_width * result_height)]
+    result = Image.new(image.mode, (result_width, result_height))
 
     for i in range(result_height):
         for j in range(result_width):
             if (i // factor == 0) and (j // factor == 0):
-                result_pixels[i * result_width + j] = image.getpixel((i // factor, j // factor))
+                result.putpixel((j, i), image.getpixel((i // factor, j // factor)))
                 continue
 
             prev_row_number = i // factor
@@ -65,7 +62,7 @@ def bilinear_interpolation_upsampling(image, factor):
             next_col_number = prev_col_number + 1
 
             if (next_row_number >= image.height) or (next_col_number >= image.width):
-                result_pixels[i * result_width + j] = (0, 0, 0)
+                result.putpixel((j, i), (0, 0, 0))
                 continue
 
             prev_row_prev_col_pixel = image.getpixel((prev_col_number, prev_row_number))
@@ -76,10 +73,7 @@ def bilinear_interpolation_upsampling(image, factor):
             upper_pseudo_pixel = math.rgb_color_lerp(prev_col_number, prev_row_prev_col_pixel, next_col_number, prev_row_next_col_pixel, j / factor)
             lower_pseudo_pixel = math.rgb_color_lerp(prev_col_number, next_row_prev_col_pixel, next_col_number, next_row_next_col_pixel, j / factor)
 
-            result_pixels[i * result_width + j] = math.rgb_color_lerp(prev_row_number, upper_pseudo_pixel, next_row_number, lower_pseudo_pixel, i / factor)
-
-    result = Image.new(image.mode, (result_width, result_height))
-    result.putdata(result_pixels)
+            result.putpixel((j, i), math.rgb_color_lerp(prev_row_number, upper_pseudo_pixel, next_row_number, lower_pseudo_pixel, i / factor))
 
     return result
 
@@ -101,24 +95,20 @@ def decimation_downsampling(image, factor):
 
     result_width = image.width // factor
     result_height = image.height // factor
-    result_pixels = [None for _ in range(result_width * result_height)]
-
-    decimated_lines = random_sample(range(image.height), image.height - result_height)
-    decimated_columns = random_sample(range(image.width), image.width - result_width)
+    result = Image.new(image.mode, (result_width, result_height))
 
     iterator = 0
-    for i in range(image.height):
-        if i in decimated_lines:
+    for i in range(image.height - factor + 1):
+        if i % factor != 0:
             continue
 
-        for j in range(image.width):
-            if j in decimated_columns:
+        for j in range(image.width - factor + 1):
+            if j % factor != 0:
                 continue
 
-            result_pixels[iterator] = image.getpixel((j, i))
+            col = iterator % result_width
+            row = iterator // result_width
+            result.putpixel((col, row), image.getpixel((j, i)))
             iterator += 1
-
-    result = Image.new(image.mode, (result_width, result_height))
-    result.putdata(result_pixels)
 
     return result
--- first_lab/task.py
+++ first_lab/task.py
@@ -7,10 +7,10 @@ downsampled_integer_number_of_times_image_path = 'images/cool_downsampled_n.jpg'
 oversampled_two_pass_image_path = 'images/cool_oversampled_two_pass.jpg'
 oversampled_one_pass_image_path = 'images/cool_oversampled_one_pass.jpg'
 
-m = 3
-n = 2
+upsample_factor = 3
+downsample_factor = 4
 
 im = Image.open(image_path)
-sampling.bilinear_interpolation_upsampling(im, m).save(upsampled_integer_number_of_times_image_path)
-sampling.decimation_downsampling(im, n).save(downsampled_integer_number_of_times_image_path)
-sampling.decimation_downsampling(sampling.bilinear_interpolation_upsampling(im, m), n).save(oversampled_two_pass_image_path)
+sampling.bilinear_interpolation_upsampling(im, upsample_factor).save(upsampled_integer_number_of_times_image_path)
+sampling.decimation_downsampling(im, downsample_factor).save(downsampled_integer_number_of_times_image_path)
+sampling.decimation_downsampling(sampling.bilinear_interpolation_upsampling(im, upsample_factor), downsample_factor).save(oversampled_two_pass_image_path)
