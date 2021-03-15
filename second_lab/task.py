6aac2a394c4eab01accd5cd14bd3f8d8dd9adad4
alexboost222 <alexboost222@gmail.com>
2021-03-15 17:04:16 +0300

    upd: second lab to sys argv

--- second_lab/task.py
+++ second_lab/task.py
@@ -1,21 +1,23 @@
 import os
+import sys
 
 from core.grayscale import grayscale
 from core.filtration import filtration
 from PIL import Image
 
-IMAGE_NAME = 'old_text_2'
-IMAGE_FORMAT = 'jpg'
-IMAGE_MODE = 'RGB'
+
 IMAGES_FOLDER_NAME = 'images'
-IMAGE_PATH = f'../{IMAGES_FOLDER_NAME}/{IMAGE_NAME}.{IMAGE_FORMAT}'
 
 
-def first_part_spatial_smooth(image):
-    image_filtration_folder_path = f'../{IMAGES_FOLDER_NAME}/{IMAGE_NAME}_processed/filtration'
+def image_path(image_name, image_format):
+    return f'../{IMAGES_FOLDER_NAME}/{image_name}.{image_format}'
 
-    spatial_smoothed_image_path = f'{image_filtration_folder_path}/{IMAGE_NAME}_spatial_smoothed.{IMAGE_FORMAT}'
-    spatial_smoothed_difference_image_path = f'{image_filtration_folder_path}/{IMAGE_NAME}_spatial_smoothed_difference.{IMAGE_FORMAT}'
+
+def first_part_spatial_smooth(image, image_name, image_format):
+    image_filtration_folder_path = f'../{IMAGES_FOLDER_NAME}/{image_name}_processed/filtration'
+
+    spatial_smoothed_image_path = f'{image_filtration_folder_path}/{image_name}_spatial_smoothed.{image_format}'
+    spatial_smoothed_difference_image_path = f'{image_filtration_folder_path}/{image_name}_spatial_smoothed_difference.{image_format}'
 
     os.makedirs(image_filtration_folder_path, exist_ok=True)
     grayscaled = grayscale.mean_grayscale(image)
@@ -23,5 +25,11 @@ def first_part_spatial_smooth(image):
     filtration.spatial_smoothing_difference(grayscaled).save(spatial_smoothed_difference_image_path)
 
 
-im = Image.open(IMAGE_PATH).convert(IMAGE_MODE)
-first_part_spatial_smooth(im)
+def main():
+    im_name, im_format = sys.argv[1].split('.')
+    im = Image.open(image_path(im_name, im_format)).convert('RGB')
+    first_part_spatial_smooth(im, im_name, im_format)
+
+
+if __name__ == '__main__':
+    main()
