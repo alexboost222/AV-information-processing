8a75b2eaa972c60e596c5ad8ce900ff18741e73b
alexboost222 <alexboost222@gmail.com>
2021-06-05 04:04:04 +0300

    add: reworked lab 4

--- lab_4/task.py
+++ lab_4/task.py
@@ -1,10 +1,14 @@
+import os
 import sys
 import matplotlib.pyplot as pyplot
 
 from PIL import Image, ImageFont, ImageDraw
 
+import folder_helper
+from core.constants import constants
+from core.helpers import helpers
 from core.sampling.sampling import cut_empty_rows_and_cols
-from folder_helper import IMAGES_FOLDER_NAME, REPORTS_FOLDER_NAME, PROJECTIONS_FOLDER_NAME
+from folder_helper import IMAGES_FOLDER_NAME, REPORTS_FOLDER_NAME
 from mdutils.mdutils import MdUtils
 
 from core.thresholding.thresholding import simple_threshold
@@ -15,31 +19,29 @@ from core.feature_extraction.feature_extraction import black_weight, normalized_
 
 
 ALPHABET = 'AΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ'
-REPORT_FILE_NAME = 'forth_lab_report'
-REPORT_FILE_TITLE = 'Лабораторная работа 4. Выделение признаков символов'
-THRESHOLD = 100
-GRAYSCALE_MODE = 'L'
 WHITE = 255
 FONT_SIZE = 52
 FONT = ImageFont.truetype(font='../fonts/times.ttf', size=FONT_SIZE)
 
 
-def generate_report(alphabet_folder_name):
-    report = MdUtils(file_name=f'../{REPORTS_FOLDER_NAME}/{REPORT_FILE_NAME}')
-    report.new_header(level=1, title=REPORT_FILE_TITLE)
+def generate_report():
+    report = MdUtils(file_name=f'./report.md')
+    report.new_header(level=1, title='Выделение признаков символов')
     report.new_line(text='Выполнил Ахманов Алексей Б18-514')
-    report.new_line(text='Алфавит - греческие заглавные')
+    report.new_line(text=f'Алфавит - {ALPHABET}')
 
     for letter in ALPHABET:
-        letter_image_path = f'../{IMAGES_FOLDER_NAME}/{alphabet_folder_name}/{letter}.png'
-        letter_image = Image.new(mode=GRAYSCALE_MODE, size=(FONT_SIZE, FONT_SIZE), color=WHITE)
-        result = ImageDraw.Draw(im=letter_image, mode=GRAYSCALE_MODE)
+        letter_folder_path = f'./{folder_helper.IMAGES_FOLDER_PATH}/letter_{letter}'
+        os.makedirs(letter_folder_path, exist_ok=True)
+        letter_image_path = f'{letter_folder_path}/{letter}.png'
+        letter_image = Image.new(mode=constants.GRAYSCALE_MODE, size=(FONT_SIZE, FONT_SIZE), color=WHITE)
+        result = ImageDraw.Draw(im=letter_image, mode=constants.GRAYSCALE_MODE)
         result.text(xy=(0, 0), text=letter, font=FONT, fill=0, anchor='lt')
         letter_image = cut_empty_rows_and_cols(letter_image)
         letter_image.save(letter_image_path)
         report.new_header(level=2, title=f'Буква {letter}')
         report.new_line(report.new_inline_image(text=letter, path=letter_image_path))
-        thresholded = simple_threshold(letter_image, THRESHOLD)
+        thresholded = simple_threshold(letter_image, 100)
         report.new_line(text=f'Вес черного - {black_weight(thresholded)}')
         report.new_line(text=f'Удельный вес черного - {normalized_black_weight(thresholded)}')
         center = gravity_center(thresholded)
@@ -48,13 +50,15 @@ def generate_report(alphabet_folder_name):
         report.new_line(text=f'Нормированные координаты центра масс - ({normalized_center[0]}, {normalized_center[1]})')
         report.new_line(text=f'Центральный горизонтальный осевой момент - {central_horizontal_axial_moment(thresholded)}')
         report.new_line(text=f'Центральный вертикальный осевой момент - {central_vertical_axial_moment(thresholded)}')
-        report.new_line(text=f'Нормированный центральный горизонтальный осевой момент - {normalized_central_horizontal_axial_moment(thresholded)}')
-        report.new_line(text=f'Нормированный центральный вертикальный осевой момент - {normalized_central_vertical_axial_moment(thresholded)}')
+        report.new_line(text=f'Нормированный центральный горизонтальный осевой момент -'
+                             f'{normalized_central_horizontal_axial_moment(thresholded)}')
+        report.new_line(text=f'Нормированный центральный вертикальный осевой момент -'
+                             f'{normalized_central_vertical_axial_moment(thresholded)}')
 
         h_levels, h_projections = horizontal_projection(thresholded)
         pyplot.plot(h_levels, h_projections)
         pyplot.title(f'Horizontal projection {letter}')
-        path = f'../{PROJECTIONS_FOLDER_NAME}/horizontal_projection_{letter}.png'
+        path = f'{letter_folder_path}/horizontal_projection_{letter}.png'
         pyplot.savefig(path)
         pyplot.close()
 
@@ -63,7 +67,7 @@ def generate_report(alphabet_folder_name):
         v_levels, v_projections = vertical_projection(thresholded)
         pyplot.plot(v_levels, v_projections)
         pyplot.title(f'Vertical projection {letter}')
-        path = f'../{PROJECTIONS_FOLDER_NAME}/vertical_projection_{letter}.png'
+        path = f'{letter_folder_path}/vertical_projection_{letter}.png'
         pyplot.savefig(path)
         pyplot.close()
 
@@ -75,8 +79,7 @@ def generate_report(alphabet_folder_name):
 
 
 def main():
-    alphabet_folder_name = sys.argv[1]
-    generate_report(alphabet_folder_name)
+    generate_report()
 
 
 if __name__ == '__main__':
